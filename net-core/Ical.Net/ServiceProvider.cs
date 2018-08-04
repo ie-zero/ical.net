cd5674004fcdccc0c7038aa6e349da8e85020dab
I. Evangelinos <ie-zero@users.noreply.github.com>
2018-08-04 11:29:46 +0100

    Format code on ServiceProvider class. No funcional changes.

--- net-core/Ical.Net/ServiceProvider.cs
+++ net-core/Ical.Net/ServiceProvider.cs
@@ -7,29 +7,27 @@ namespace Ical.Net
 {
     public class ServiceProvider
     {
-        private readonly IDictionary<Type, object> _mTypedServices = new Dictionary<Type, object>();
-        private readonly IDictionary<string, object> _mNamedServices = new Dictionary<string, object>();
+        private readonly IDictionary<string, object> _namedServices = new Dictionary<string, object>();
+        private readonly IDictionary<Type, object> _typedServices = new Dictionary<Type, object>();
 
         public virtual object GetService(Type serviceType)
         {
-            object service;
-            _mTypedServices.TryGetValue(serviceType, out service);
+            _typedServices.TryGetValue(serviceType, out object service);
             return service;
         }
 
         public virtual object GetService(string name)
         {
-            object service;
-            _mNamedServices.TryGetValue(name, out service);
+            _namedServices.TryGetValue(name, out object service);
             return service;
         }
 
         public virtual T GetService<T>()
         {
-            var service = GetService(typeof (T));
+            var service = GetService(typeof(T));
             if (service is T)
             {
-                return (T) service;
+                return (T)service;
             }
             return default(T);
         }
@@ -39,16 +37,41 @@ namespace Ical.Net
             var service = GetService(name);
             if (service is T)
             {
-                return (T) service;
+                return (T)service;
             }
             return default(T);
+        }
+
+        public virtual void RemoveService(Type type)
+        {
+            if (type != null)
+            {
+                if (_typedServices.ContainsKey(type))
+                {
+                    _typedServices.Remove(type);
+                }
+
+                // Get interfaces for the given type
+                foreach (var iface in type.GetInterfaces().Where(iface => _typedServices.ContainsKey(iface)))
+                {
+                    _typedServices.Remove(iface);
+                }
+            }
+        }
+
+        public virtual void RemoveService(string name)
+        {
+            if (_namedServices.ContainsKey(name))
+            {
+                _namedServices.Remove(name);
+            }
         }
 
         public virtual void SetService(string name, object obj)
         {
             if (!string.IsNullOrEmpty(name) && obj != null)
             {
-                _mNamedServices[name] = obj;
+                _namedServices[name] = obj;
             }
         }
 
@@ -57,38 +80,13 @@ namespace Ical.Net
             if (obj != null)
             {
                 var type = obj.GetType();
-                _mTypedServices[type] = obj;
+                _typedServices[type] = obj;
 
                 // Get interfaces for the given type
                 foreach (var iface in type.GetInterfaces())
                 {
-                    _mTypedServices[iface] = obj;
+                    _typedServices[iface] = obj;
                 }
-            }
-        }
-
-        public virtual void RemoveService(Type type)
-        {
-            if (type != null)
-            {
-                if (_mTypedServices.ContainsKey(type))
-                {
-                    _mTypedServices.Remove(type);
-                }
-
-                // Get interfaces for the given type
-                foreach (var iface in type.GetInterfaces().Where(iface => _mTypedServices.ContainsKey(iface)))
-                {
-                    _mTypedServices.Remove(iface);
-                }
-            }
-        }
-
-        public virtual void RemoveService(string name)
-        {
-            if (_mNamedServices.ContainsKey(name))
-            {
-                _mNamedServices.Remove(name);
             }
         }
     }
