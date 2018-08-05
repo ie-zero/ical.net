b1b9269da7811d48ed6d567cba83adb6bc1cebe7
I. Evangelinos <ie-zero@users.noreply.github.com>
2018-08-05 09:59:37 +0100

    Format code on ICalendarDataType interface. No funcional changes

--- net-core/Ical.Net/DataTypes/ICalendarDataType.cs
+++ net-core/Ical.Net/DataTypes/ICalendarDataType.cs
@@ -4,11 +4,12 @@ namespace Ical.Net.DataTypes
 {
     public interface ICalendarDataType : ICalendarParameterCollectionContainer, ICopyable, IServiceProvider
     {
-        Type GetValueType();
-        void SetValueType(string type);
         ICalendarObject AssociatedObject { get; set; }
         Calendar Calendar { get; }
-
         string Language { get; set; }
+
+        // TODO: GetValueType() and SetValueType() are candidates to be extracted in separate interface.
+        Type GetValueType();
+        void SetValueType(string type);
     }
 }
