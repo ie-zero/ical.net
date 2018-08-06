39967f5e52db05c8b36e1a8f255b4f4cba5c2414
I. Evangelinos <ie-zero@users.noreply.github.com>
2018-08-06 18:46:59 +0100

    Remove dependency of ISerializer on IServiceProvider

--- net-core/Ical.Net/Serialization/ISerializer.cs
+++ net-core/Ical.Net/Serialization/ISerializer.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Ical.Net.Serialization
 {
-    public interface ISerializer : IServiceProvider
+    public interface ISerializer 
     {
         SerializationContext SerializationContext { get; }
         Type TargetType { get; }
