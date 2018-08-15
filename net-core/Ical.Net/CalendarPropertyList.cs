e2d7f5e2f06305376e13553da2107cb9c03e2c2f
I. Evangelinos <ie-zero@users.noreply.github.com>
2018-08-15 21:37:25 +0100

    Encapsulate further CalendarPropertyList class

--- net-core/Ical.Net/CalendarPropertyList.cs
+++ net-core/Ical.Net/CalendarPropertyList.cs
@@ -1,19 +1,23 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Ical.Net.Collections;
 
 namespace Ical.Net
 {
-    public class CalendarPropertyList : GroupedValueList<CalendarProperty, object>
+    public class CalendarPropertyList : IEnumerable<CalendarProperty>
     {
         private readonly ICalendarObject _parent;
+        private readonly GroupedValueList<CalendarProperty, object> _list;
 
         public CalendarPropertyList(ICalendarObject parent)
         {
             _parent = parent;
-            ItemAdded += CalendarPropertyList_ItemAdded;
+            _list = new GroupedValueList<CalendarProperty, object>();
+            _list.ItemAdded += ItemAdded;
         }
 
-        private void CalendarPropertyList_ItemAdded(object sender, ItemProcessedEventArgs<CalendarProperty> e)
+        private void ItemAdded(object sender, ItemProcessedEventArgs<CalendarProperty> e)
         {
             e.Item.Parent = _parent;
         }
@@ -24,6 +28,61 @@ namespace Ical.Net
             {
                 return ContainsKey(name) ? AllOf(name).FirstOrDefault() : null;
             }
+        }
+
+        public IEnumerable<CalendarProperty> AllOf(string group)
+        {
+            return _list.AllOf(group);
+        }
+
+        public bool ContainsKey(string group)
+        {
+            return _list.ContainsKey(group);
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public void Add(CalendarProperty property)
+        {
+            _list.Add(property);
+        }
+
+        public bool Remove(string group)
+        {
+            return _list.Remove(group);
+        }
+
+        public void Set(string group, object value)
+        {
+            _list.Set(group, value);
+        }
+
+        public void Set(string group, IEnumerable<object> values)
+        {
+            _list.Set(group, values);
+        }
+
+        public T Get<T>(string group)
+        {
+            return _list.Get<T>(group);
+        }
+
+        public IList<T> GetMany<T>(string group)
+        {
+            return _list.GetMany<T>(group);
+        }
+
+        public IEnumerator<CalendarProperty> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
