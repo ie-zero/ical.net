496b5a170ab08e0cc84a623e3e861eea6821bd1e
I. Evangelinos <ievangelinos@outlook.com>
2018-08-20 20:49:59 +0100

    Improve encapsulation on AlarmOccurrence class
    Remove properties and property setters on AlarmOccurrence class to improve encapsulation. This class is now closer to a value object.

--- net-core/Ical.Net/DataTypes/AlarmOccurrence.cs
+++ net-core/Ical.Net/DataTypes/AlarmOccurrence.cs
@@ -6,17 +6,17 @@ using Ical.Net.Components;
 namespace Ical.Net.DataTypes
 {
     /// <summary>
-    /// It represents a specific occurrence of an <see cref="Alarm"/>.
+    /// Represents a specific occurrence of an <see cref="Alarm"/>.
     /// </summary>
     /// <remarks>
-    /// The <see cref="AlarmOccurrence"/> contains the <see cref="Period"/> when the alarm occurs,
+    /// The <see cref="AlarmOccurrence"/> contains the date/time when the alarm occurs,
     /// the <see cref="Alarm"/> that fired, and the component on which the alarm fired.
     /// </remarks>
     public class AlarmOccurrence : IComparable<AlarmOccurrence>
     {
         public AlarmOccurrence(AlarmOccurrence ao)
         {
-            Period = ao.Period;
+            DateTime = ao.DateTime;
             Component = ao.Component;
             Alarm = ao.Alarm;
         }
@@ -24,26 +24,19 @@ namespace Ical.Net.DataTypes
         public AlarmOccurrence(Alarm alarm, IDateTime dt, IRecurringComponent rc)
         {
             Alarm = alarm;
-            Period = new Period(dt);
+            DateTime = dt;
             Component = rc;
         }
 
-        public Alarm Alarm { get; set; }
+        public Alarm Alarm { get; }
 
-        public IRecurringComponent Component { get; set; }
+        public IRecurringComponent Component { get; }
 
-        // TODO: There is a dependency between DateTime and Period properties on AlarmOccurrence.
-        public IDateTime DateTime
-        {
-            get => Period.StartTime;
-            set => Period = new Period(value);
-        }
-
-        public Period Period { get; set; }
+        public IDateTime DateTime { get; }
 
         public int CompareTo(AlarmOccurrence other)
         {
-            return Period.CompareTo(other.Period);
+            return DateTime.CompareTo(other.DateTime);
         }
 
         public override bool Equals(object obj)
@@ -74,7 +67,7 @@ namespace Ical.Net.DataTypes
             // TODO: Alarm does not implement Equals or GetHashCode()
             yield return Alarm;
             yield return Component;
-            yield return Period;
+            yield return DateTime;
         }
     }
 }
