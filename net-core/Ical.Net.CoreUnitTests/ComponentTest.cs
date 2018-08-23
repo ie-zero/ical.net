ae7b8f17d5dc2c73cc74dbbb747e32cf4ab4a66d
I. Evangelinos <ievangelinos@outlook.com>
2018-08-23 21:46:19 +0100

    Improve unit tests

--- net-core/Ical.Net.CoreUnitTests/ComponentTest.cs
+++ net-core/Ical.Net.CoreUnitTests/ComponentTest.cs
@@ -8,33 +8,39 @@ namespace Ical.Net.CoreUnitTests
     public class ComponentTest
     {
         [Test, Category("Components")]
-        public void UniqueComponent1()
+        public void VerifyUniqueComponentPropertiesOnCreate()
         {
-            var iCal = new Calendar();
-            var evt = iCal.Create<CalendarEvent>();
+            var calendar = new Calendar();
+            var component = calendar.Create<DummyUniqueComponent>();
 
-            Assert.IsNotNull(evt.Uid);
-            Assert.IsNull(evt.Created); // We don't want this to be set automatically
-            Assert.IsNotNull(evt.DtStamp);
+            Assert.IsNotNull(component.Uid);
+            Assert.IsNotNull(component.DtStamp);
+        }
+
+        private class DummyUniqueComponent : UniqueComponent
+        {
+            public DummyUniqueComponent() : base(name: "DUMMY") {}
         }
 
         [Test, Category("Components")]
         public void ChangeCalDateTimeValue()
         {
-            var e = new CalendarEvent
+            // TODO: Review this test. It does not appear to be testing anything of value. 
+
+            var evt = new CalendarEvent
             {
                 Start = new CalDateTime(2017, 11, 22, 11, 00, 01),
                 End = new CalDateTime(2017, 11, 22, 11, 30, 01),
             };
 
-            var firstStartAsUtc = e.Start.GetAsUtc();
-            var firstEndAsUtc = e.End.GetAsUtc();
+            var firstStartAsUtc = evt.Start.GetAsUtc();
+            var firstEndAsUtc = evt.End.GetAsUtc();
 
-            e.Start.Value = new DateTime(2017, 11, 22, 11, 30, 01);
-            e.End.Value = new DateTime(2017, 11, 22, 12, 00, 01);
+            evt.Start.Value = new DateTime(2017, 11, 22, 11, 30, 01);
+            evt.End.Value = new DateTime(2017, 11, 22, 12, 00, 01);
 
-            var secondStartAsUtc = e.Start.GetAsUtc();
-            var secondEndAsUtc = e.End.GetAsUtc();
+            var secondStartAsUtc = evt.Start.GetAsUtc();
+            var secondEndAsUtc = evt.End.GetAsUtc();
 
             Assert.AreNotEqual(firstStartAsUtc, secondStartAsUtc);
             Assert.AreNotEqual(firstEndAsUtc, secondEndAsUtc);
