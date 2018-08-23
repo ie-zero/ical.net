0b9be12f7e95290a63ead4728385802bb6b5890f
I. Evangelinos <ievangelinos@outlook.com>
2018-08-23 20:48:25 +0100

    Format code on GeographicLocationSerializer class. No functional changes

--- net-core/Ical.Net/Serialization/DataTypes/GeographicLocationSerializer.cs
+++ net-core/Ical.Net/Serialization/DataTypes/GeographicLocationSerializer.cs
@@ -9,53 +9,42 @@ namespace Ical.Net.Serialization.DataTypes
     {
         public GeographicLocationSerializer(SerializationContext ctx) : base(ctx) { }
 
-        public override Type TargetType => typeof (GeographicLocation);
+        public override Type TargetType => typeof(GeographicLocation);
 
         public override string SerializeToString(object obj)
         {
-            var g = obj as GeographicLocation;
-            if (g == null)
-            {
-                return null;
-            }
+            var geoLocation = obj as GeographicLocation;
+            if (geoLocation == null) { return null; }
 
-            var value = g.Latitude.ToString("0.000000", CultureInfo.InvariantCulture.NumberFormat) + ";"
-                + g.Longitude.ToString("0.000000", CultureInfo.InvariantCulture.NumberFormat);
-            return Encode(g, value);
+            var value = geoLocation.Latitude.ToString("0.000000", CultureInfo.InvariantCulture.NumberFormat) + ";"
+                      + geoLocation.Longitude.ToString("0.000000", CultureInfo.InvariantCulture.NumberFormat);
+            return Encode(geoLocation, value);
         }
 
         public GeographicLocation Deserialize(string value)
         {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                return null;
-            }
+            if (string.IsNullOrWhiteSpace(value)) { return null; }
 
-            var g = CreateAndAssociate() as GeographicLocation;
-            if (g == null)
-            {
-                return null;
-            }
+            var geoLocation = CreateAndAssociate() as GeographicLocation;
+            if (geoLocation == null) { return null; }
 
             // Decode the value, if necessary!
-            value = Decode(g, value);
+            value = Decode(geoLocation, value);
 
-            var values = value.Split(new [] {';'}, StringSplitOptions.RemoveEmptyEntries);
-            if (values.Length != 2)
-            {
-                return null;
-            }
+            var values = value.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            if (values.Length != 2) { return null; }
 
-            double lat;
-            double lon;
-            double.TryParse(values[0], NumberStyles.Any, CultureInfo.InvariantCulture, out lat);
-            double.TryParse(values[1], NumberStyles.Any, CultureInfo.InvariantCulture, out lon);
-            g.Latitude = lat;
-            g.Longitude = lon;
+            double.TryParse(values[0], NumberStyles.Any, CultureInfo.InvariantCulture, out double latitude);
+            double.TryParse(values[1], NumberStyles.Any, CultureInfo.InvariantCulture, out double longitude);
 
-            return g;
+            geoLocation.Latitude = latitude;
+            geoLocation.Longitude = longitude;
+            return geoLocation;
         }
 
-        public override object Deserialize(TextReader tr) => Deserialize(tr.ReadToEnd());
+        public override object Deserialize(TextReader reader)
+        {
+            return Deserialize(reader.ReadToEnd());
+        }
     }
 }
