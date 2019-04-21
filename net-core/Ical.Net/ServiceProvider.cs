811048e1eb377149b90da756518423fe3a4ecf76
I. Evangelinos <ie-zero@users.noreply.github.com>
2019-04-21 11:50:36 +0100

    Refacor ServiceProvider class

--- net-core/Ical.Net/ServiceProvider.cs
+++ net-core/Ical.Net/ServiceProvider.cs
@@ -7,31 +7,124 @@ namespace Ical.Net
 {
     public class ServiceProvider
     {
-        private readonly IDictionary<Type, object> _mTypedServices = new Dictionary<Type, object>();
-        private readonly IDictionary<string, object> _mNamedServices = new Dictionary<string, object>();
+        private readonly TypedServicesProvider _typedServices;
+        private readonly NamedServicesProvider _namedServices;
+
+        public ServiceProvider()
+        {
+            _typedServices = new TypedServicesProvider();
+            _namedServices = new NamedServicesProvider();
+        }
 
         public object GetService(Type serviceType)
         {
-            object service;
-            _mTypedServices.TryGetValue(serviceType, out service);
-            return service;
+            return _typedServices.GetService(serviceType);
         }
 
         public object GetService(string name)
         {
-            object service;
-            _mNamedServices.TryGetValue(name, out service);
+            return _namedServices.GetService(name);
+        }
+
+        public T GetService<T>()
+        {
+            return _typedServices.GetService<T>();
+        }
+
+        public T GetService<T>(string name)
+        {
+            return _namedServices.GetService<T>(name);
+        }
+
+        public void SetService(string name, object obj)
+        {
+            _namedServices.SetService(name, obj);
+        }
+
+        public void SetService(object obj)
+        {
+            _typedServices.SetService(obj);
+        }
+
+        public void RemoveService(Type type)
+        {
+            _typedServices.RemoveService(type);
+        }
+
+        public void RemoveService(string name)
+        {
+            _namedServices.RemoveService(name);
+        }
+    }
+
+    public class TypedServicesProvider
+    {
+        private readonly IDictionary<Type, object> _services = new Dictionary<Type, object>();
+
+        public object GetService(Type serviceType)
+        {
+            _services.TryGetValue(serviceType, out object service);
             return service;
         }
 
         public T GetService<T>()
         {
-            var service = GetService(typeof (T));
+            var service = GetService(typeof(T));
             if (service is T)
             {
-                return (T) service;
+                return (T)service;
             }
             return default(T);
+        }
+
+        /// <summary>
+        /// Stores a unique entry for the type and each interface of the supplied object. If multiple objects 
+        /// implement the same interface only the last one added will be retrievable under the interface type.
+        /// </summary>
+        public void SetService(object obj)
+        {
+            if (obj == null) { return; }
+
+            var type = obj.GetType();
+            _services[type] = obj;
+
+            // Get interfaces for the given type
+            foreach (var iface in type.GetInterfaces())
+            {
+                _services[iface] = obj;
+            }
+        }
+
+        public void RemoveService(Type type)
+        {
+            if (type != null)
+            {
+                if (_services.ContainsKey(type))
+                {
+                    _services.Remove(type);
+                }
+
+                // TODO: Validate that the wrong type cannot be removed by mistake if multiple classes are implementing the same interface.
+
+                // Get interfaces for the given type
+                foreach (var iface in type.GetInterfaces().Where(iface => _services.ContainsKey(iface)))
+                {
+                    _services.Remove(iface);
+                }
+            }
+        }
+    }
+
+
+    public class NamedServicesProvider
+    {
+        private readonly IDictionary<string, object> _services = new Dictionary<string, object>();
+
+        public object GetService(string name)
+        {
+            object service;
+            _services.TryGetValue(name, out service);
+            return service;
         }
 
         public T GetService<T>(string name)
@@ -39,7 +132,7 @@ namespace Ical.Net
             var service = GetService(name);
             if (service is T)
             {
-                return (T) service;
+                return (T)service;
             }
             return default(T);
         }
@@ -48,47 +141,15 @@ namespace Ical.Net
         {
             if (!string.IsNullOrEmpty(name) && obj != null)
             {
-                _mNamedServices[name] = obj;
-            }
-        }
-
-        public void SetService(object obj)
-        {
-            if (obj != null)
-            {
-                var type = obj.GetType();
-                _mTypedServices[type] = obj;
-
-                // Get interfaces for the given type
-                foreach (var iface in type.GetInterfaces())
-                {
-                    _mTypedServices[iface] = obj;
-                }
-            }
-        }
-
-        public void RemoveService(Type type)
-        {
-            if (type != null)
-            {
-                if (_mTypedServices.ContainsKey(type))
-                {
-                    _mTypedServices.Remove(type);
-                }
-
-                // Get interfaces for the given type
-                foreach (var iface in type.GetInterfaces().Where(iface => _mTypedServices.ContainsKey(iface)))
-                {
-                    _mTypedServices.Remove(iface);
-                }
+                _services[name] = obj;
             }
         }
 
         public void RemoveService(string name)
         {
-            if (_mNamedServices.ContainsKey(name))
+            if (_services.ContainsKey(name))
             {
-                _mNamedServices.Remove(name);
+                _services.Remove(name);
             }
         }
     }
