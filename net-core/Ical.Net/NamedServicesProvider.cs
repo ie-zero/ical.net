ec46541f49cf53b0e797e8d6e852829b822ece9e
I. Evangelinos <ie-zero@users.noreply.github.com>
2019-04-21 12:35:17 +0100

    Eliminate the ServiceProvider class as obsolete

---
+++ net-core/Ical.Net/NamedServicesProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Ical.Net
+{
+    public class NamedServicesProvider
+    {
+        private readonly IDictionary<string, object> _services = new Dictionary<string, object>();
+
+        public object GetService(string name)
+        {
+            object service;
+            _services.TryGetValue(name, out service);
+            return service;
+        }
+
+        public T GetService<T>(string name)
+        {
+            var service = GetService(name);
+            if (service is T)
+            {
+                return (T)service;
+            }
+            return default(T);
+        }
+
+        public void SetService(string name, object obj)
+        {
+            if (!string.IsNullOrEmpty(name) && obj != null)
+            {
+                _services[name] = obj;
+            }
+        }
+
+        public void RemoveService(string name)
+        {
+            if (_services.ContainsKey(name))
+            {
+                _services.Remove(name);
+            }
+        }
+    }
+}
